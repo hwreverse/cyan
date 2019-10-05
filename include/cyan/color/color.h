9b02efbf0b12e14c9c99e22c3f211ebdea592bb7
Sebastien Druon <seb@homedruon.com>
2019-10-05 11:45:07 +0200

    added a fex color conversion functions

--- include/cyan/color/color.h
+++ include/cyan/color/color.h
@@ -43,14 +43,45 @@ enum {
     CYAN_RGB_SMPTE_C         =13,
     CYAN_RGB_sRGB            =14,
     CYAN_RGB_WIDE_GAMUT      =15
-} cyan_rgb_workingspace ;
+} cyan_rgb_workspace ;
 
+double cyan_refwhite_xyz[11][3] = { { 1.09850 , 1.00000 , 0.35585 },    // CYAN_A
+                                    { 0.99072 , 1.00000 , 0.85223 },    // CYAN_B
+                                    { 0.98074 , 1.00000 , 1.18232 },    // CYAN_C
+                                    { 0.96422 , 1.00000 , 0.82521 },    // CYAN_D50
+                                    { 0.95682 , 1.00000 , 0.92149 },    // CYAN_D55
+                                    { 0.95047 , 1.00000 , 1.08883 },    // CYAN_D65
+                                    { 0.94972 , 1.00000 , 1.22638 },    // CYAN_D75
+                                    { 1.00000 , 1.00000 , 1.00000 },    // CYAN_E
+                                    { 0.99186 , 1.00000 , 0.67393 },    // CYAN_F2
+                                    { 0.95041 , 1.00000 , 1.08747 },    // CYAN_F7
+                                    { 1.00962 , 1.00000 , 0.64350 } };  // CYAN_F11
 
+void RGB_to_XYZ( enum cyan_rgb_worskpace RGB_space, double R, double G, double B, enum cyan_refwhite w, double* X, double* Y, double* Z ) ;  // TODO
+void XYZ_to_RGB( enum cyan_refwhite w, double X, double Y, double Z, enum cyan_rgb_workspage RGB_space, double* R, double* G, double* B ) ;  // TODO
 
+void xyY_to_XYZ() ;      // TODO
+void XYZ_to_xyY() ;      // TODO
 
+void Lab_to_XYZ() ;      // TODO
+void XYZ_to_Lab() ;      // TODO
 
+void LCHab_to_Lab() ;    // TODO
+void Lab_to_LCHab() ;    // TODO
 
+void Lab_to_XYZ() ;      // TODO
+void XYZ_to_Lab() ;      // TODO
 
+void Luv_to_XYZ() ;      // TODO
+void XYZ_to_Luv() ;      // TODO
 
+void LCHuv_to_Luv() ;    // TODO
+void Luv_to_LCHuv() ;    // TODO
+
+void Luv_to_XYZ() ;      // TODO
+void XYZ_to_Luv() ;      // TODO
+
+void spectrum_to_XYZ() ;    // TODO
+void XYZ_to_spectrum() ;    // TODO
 
 #endif
