f7137b90d9af0f53098be529801c20785b55303c
DavyL <cr4zypxl@gmail.com>
2020-02-14 12:18:22 +0100

    Forgot tot add files?

---
+++ tools/cyan_imageviewer/src/load_png.h
@@ -0,0 +1,14 @@
+#ifndef LOAD_PNG_H
+#define LOAD_PNG_H
+
+#include <stdio.h>
+
+#include <cyan/color/color.h>
+#include <cyan/image/image.h>
+
+#include <png.h>
+
+image_t* png2image(FILE *) ; 
+//TODO Add image2png() ?
+#endif
+
---
+++ tools/cyan_transforms/src/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include <cyan/color/color.h>
+#include <cyan/image/image.h>
+#include <cyan/image/load_png.h>
+
+
+int main( int argc, char** argv, char* envv ) {
+
+	int i, j ;
+	int result ;
+	image_t* image ; 
+	FILE * fp;
+
+	fp = fopen("lena.png", "r");
+	if(fp == NULL){	
+		fprintf(stderr, "Couldn't open file.\n Error : %d, (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+
+	if (argc != 2 ) {
+		fprintf(stderr,"Usage : %s output_file.ppm\n", argv[0] ) ;
+		return -1 ;
+	}
+	image = png2image(fp);
+	if ( image == NULL ) {
+		fprintf(stderr,"image allocation failed\n") ;
+		return -1 ;
+	}
+	result = image_save_ppm( image, argv[1] ) ; 
+
+	if (result != 0) {
+		fprintf(stderr, "cannot save file\n") ;
+		return -1 ;
+	}
+
+	image_free( image) ;
+
+	fclose(fp);
+
+    return 0 ;
+}
+
+
