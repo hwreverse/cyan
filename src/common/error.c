c575f897babd83e025667b4f27477bccc7c85934
Sebastien Druon <seb@homedruon.com>
2020-03-12 11:15:30 +0100

    support des traductions ok dans le CMakeLists.txt. reste le code en C

--- src/common/error.c
+++ src/common/error.c
@@ -1,15 +1,17 @@
 #include "cyan/common/error.h"
 
+#define _(STRING) STRING
+
 const char* error_get_text ( int error_code ) {
     switch ( error_code ) {
         case ERR_OK:
-            return "OK" ;
+            return _("OK") ;
         case ERR_MALLOC:
-            return "Memory allocation failed" ;    
+            return _("Memory allocation failed") ;    
         case ERR_NULL:
-            return "NULL pointer was given" ;
+            return _("NULL pointer was given") ;
         case ERR_INVALID_ARG:
-            return "Given argument is invalid or has wrong value" ;
+            return _("Given argument is invalid or has wrong value") ;
     }
-    return "Unknown error" ;
+    return _("Unknown error") ;
 }
