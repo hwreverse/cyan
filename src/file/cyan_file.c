535e4daefa1ae3fe213f08582eaef553fbec9dcb
Sebastien Druon <seb@homedruon.com>
2020-05-25 00:43:36 +0200

    added version number to filetype

--- src/file/cyan_file.c
+++ src/file/cyan_file.c
@@ -1,12 +1,24 @@
 #include <string.h>
 #include "cyan/common/error.h"
+#include "cyan/common/config.h"
 #include "cyan_file.h"
 
 
 int write_header( FILE* fd, const char header[] ) {
     int ret ;
+    unsigned char version[2] ;
+    
     ret=fwrite( header, strlen(header), 1, fd ) ;
     if ( ret != 1 ) {
+        CYAN_ERROR( ERR_FILE ) ;
+        return ERR_FILE ;
+    }
+    
+    version[0] = CYAN_VERSION_MAJOR ;
+    version[1] = CYAN_VERSION_MINOR ;
+
+    ret=fwrite( version, sizeof(unsigned char), 2, fd ) ;
+    if ( ret != 2 ) {
         CYAN_ERROR( ERR_FILE ) ;
         return ERR_FILE ;
     }
@@ -15,9 +27,12 @@ int write_header( FILE* fd, const char header[] ) {
 }
 
 int read_header( FILE* fd, const char header[] ) {
+    int ret ;
     char buff[10]={'\0'} ;
+    unsigned char version[2] ;
     
-    if ( fread(buff, strlen(header),1,fd ) != strlen(header) ) {
+    ret = fread(buff, strlen(header),1,fd ) ;
+    if ( ret != 1 ) {
         CYAN_ERROR( ERR_FILE ) ;
         return ERR_FILE ;
     }
@@ -26,6 +41,20 @@ int read_header( FILE* fd, const char header[] ) {
         CYAN_ERROR( ERR_FILE ) ;
         return ERR_FILE ;
     }
+
+    ret = fread(version, sizeof(unsigned char), 2, fd ) ;
+    if ( ret != 2 ) {
+        CYAN_ERROR( ERR_FILE ) ;
+        return ERR_FILE ;
+    }
+    
+    if ((version[0]!=CYAN_VERSION_MAJOR)||(version[1]!=CYAN_VERSION_MINOR)) {
+        fprintf(stderr,"Cannot read file : Cyan version is not the same \n") ;
+        CYAN_ERROR( ERR_FILE ) ;
+        return ERR_FILE ;
+    }
+
+
 
     return ERR_OK ;
 
